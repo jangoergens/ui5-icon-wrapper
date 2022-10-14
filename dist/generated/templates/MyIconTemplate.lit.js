e5cc6e3aa0fef7ea6f0f4761458cf5f2fce81c21
jangoergens <22739836+jangoergens@users.noreply.github.com>
2022-10-14 14:49:49 +0200

    Fix file name

---
+++ dist/generated/templates/MyIconTemplate.lit.js
@@ -0,0 +1,9 @@
+/* eslint no-unused-vars: 0 */
+import { html, svg, repeat, classMap, styleMap, ifDefined, unsafeHTML, scopeTag } from "@ui5/webcomponents-base/dist/renderer/LitRenderer.js";
+
+
+		let block0 = () => {
+			return svg`<g><path d="M150 0 L75 200 L225 200 Z" /></g>; `
+		}
+
+export default block0;
